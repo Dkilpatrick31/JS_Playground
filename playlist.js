d85b1d32bd216e6bf1b1f6cd0871ccbb73f9e2ab
Dane Kilpatrick <danekilpatrick@Danes-Air.localdomain>
2019-01-29 15:43:14 -0600

    Built a 2-deminsional array for a list of songs and the artist for those songs | Done in playlist.js

--- playlist.js
+++ playlist.js
@@ -61,25 +61,49 @@
 // var position = fruits.indexOf('banana');
 // console.log(position);
 
-var inStock = ['apple', 'eggs', 'milk', 'cookie', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onions', 'tofu', 'frozen dinner', 'cucumber'];
+// var inStock = ['apple', 'eggs', 'milk', 'cookie', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onions', 'tofu', 'frozen dinner', 'cucumber'];
+//
+// var search;
+// function print(message) {
+//   document.write('<p>' + message + '</p>');
+// }
+//
+// while (true) {
+//   search = prompt("Search for a product in our store. Type 'list' to see all of the produce and 'quit' to exit.");
+//   search = search.toLowerCase();
+//   if (search === 'quit') {
+//     break;
+//   } else if (search === 'list') {
+//     print(inStock.join(', '));
+//   } else {
+//     if (inStock.indexOf(search) > -1) {
+//       print('Yes, we have ' + search + ' in the store.');
+//     } else {
+//       print(search + ' is not inl stock.');
+//     }
+//   }
+// }
 
-var search;
+var playlist = [
+  ['I Did It My Way', ' Frank Sinatra'],
+  ['Respect', 'Aretha Frankline'],
+  ['Imagine', 'John Lennon'],
+  ['Born to Run', 'Bruce Springsteen'],
+  ['Louie Louie', 'The Kingsmen'],
+  ['Maybellene', 'Chuck Berry']
+];
+
 function print(message) {
-  document.write('<p>' + message + '</p>');
+  document.write(message);
 }
 
-while (true) {
-  search = prompt("Search for a product in our store. Type 'list' to see all of the produce and 'quit' to exit.");
-  search = search.toLowerCase();
-  if (search === 'quit') {
-    break;
-  } else if (search === 'list') {
-    print(inStock.join(', '));
-  } else {
-    if (inStock.indexOf(search) > -1) {
-      print('Yes, we have ' + search + ' in the store.');
-    } else {
-      print(search + ' is not in stock.');
-    }
+function printSongs(songs) {
+  var listHTML = '<ol>';
+  for (var i = 0; i < songs.length; i += 1) {
+    listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
   }
+  listHTML += '</ol>';
+  print(listHTML);
 }
+
+printSongs(playlist);
