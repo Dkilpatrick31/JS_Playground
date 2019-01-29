93c83ad79607776ff97eb071c7a3e1b6c76edc0b
Dane Kilpatrick <danekilpatrick@Danes-Air.localdomain>
2019-01-29 15:16:23 -0600

    Created a while loop to be able to search through a grocery store and see if they have certain products | Added if else conditions to print thesearches to the page. | All done in playlist.js

--- playlist.js
+++ playlist.js
@@ -35,28 +35,51 @@
 //   print(listHTML);
 // }
 // printList(playlist);
-var playlist = [
-  "Louie Louie",
-  "Maybellene",
-  "Born to Run",
-  "I did it my way",
-  "Respect",
-  "Imagine"
-];
-var playlistString = playlist.join(', ');
-console.log(playlistString);
+// var playlist = [
+//   "Louie Louie",
+//   "Maybellene",
+//   "Born to Run",
+//   "I did it my way",
+//   "Respect",
+//   "Imagine"
+// ];
+// var playlistString = playlist.join(', ');
+// console.log(playlistString);
+//
+// var friends = [
+//   'Dane',
+//   'Dj',
+//   'Gagle',
+//   'Tyle',
+// ];
+//
+// var airForceFriends = ['Pat', 'Lombard', 'Hartman', 'Katie'];
+// var friendsTogether = friends.concat(airForceFriends); //.concat adds two arrays together
+// console.log(friendsTogether);
+//
+// var fruits = ['apple', 'banana', 'orange', 'grape'];
+// var position = fruits.indexOf('banana');
+// console.log(position);
 
-var friends = [
-  'Dane',
-  'Dj',
-  'Gagle',
-  'Tyle',
-];
+var inStock = ['apple', 'eggs', 'milk', 'cookie', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onions', 'tofu', 'frozen dinner', 'cucumber'];
 
-var airForceFriends = ['Pat', 'Lombard', 'Hartman', 'Katie'];
-var friendsTogether = friends.concat(airForceFriends); //.concat adds two arrays together
-console.log(friendsTogether);
+var search;
+function print(message) {
+  document.write('<p>' + message + '</p>');
+}
 
-var fruits = ['apple', 'banana', 'orange', 'grape'];
-var position = fruits.indexOf('banana');
-console.log(position);
+while (true) {
+  search = prompt("Search for a product in our store. Type 'list' to see all of the produce and 'quit' to exit.");
+  search = search.toLowerCase();
+  if (search === 'quit') {
+    break;
+  } else if (search === 'list') {
+    print(inStock.join(', '));
+  } else {
+    if (inStock.indexOf(search) > -1) {
+      print('Yes, we have ' + search + ' in the store.');
+    } else {
+      print(search + ' is not in stock.');
+    }
+  }
+}
