197ab991ff5367947a6b899074962e7b1412cbcc
Dane Kilpatrick <danekilpatrick@Danes-Air.localdomain>
2019-01-29 14:17:55 -0600

    Played around with .concat & .indexOf methods in the playlist.js file

--- playlist.js
+++ playlist.js
@@ -1,6 +1,6 @@
 // var musicPlaylist = ["Louie Louie", "Maybellene", "Born to Run", "I did it my way", "Respect", "Imagine"];
 //
-// var secondMusicPlaylist = [
+// var playlist = [
 //   "Louie Louie",
 //   "Maybellene",
 //   "Born to Run",
@@ -8,13 +8,55 @@
 //   "Respect",
 //   "Imagine"
 // ];
+
 //
 // console.log(musicPlaylist);
 // musicPlaylist.push('Hello World');
 // console.log(musicPlaylist);
 // console.log(musicPlaylist.length);
+//
+// var playList = [];
+// playList.push('I did it my way');
+// playList.push('Imagine', 'Respect');
+// playList.unshift('Born To Run');
+// playList.unshift('Louie Louie', 'Maybellene');
+// printList(playList);
+//
+// function print(message) {
+//   document.write(message);
+// }
+//
+// function printList(list) {
+//   var listHTML = '<ol>';
+//   for (var i = 0; i < list.length; i += 1) {
+//     listHTML += '<li>' + list[i] + '<li>';
+//   }
+//   listHTML += '</ol>';
+//   print(listHTML);
+// }
+// printList(playlist);
+var playlist = [
+  "Louie Louie",
+  "Maybellene",
+  "Born to Run",
+  "I did it my way",
+  "Respect",
+  "Imagine"
+];
+var playlistString = playlist.join(', ');
+console.log(playlistString);
 
-var playList = [];
-playList.push('I did it my way');
+var friends = [
+  'Dane',
+  'Dj',
+  'Gagle',
+  'Tyle',
+];
 
-printList(playList);
+var airForceFriends = ['Pat', 'Lombard', 'Hartman', 'Katie'];
+var friendsTogether = friends.concat(airForceFriends); //.concat adds two arrays together
+console.log(friendsTogether);
+
+var fruits = ['apple', 'banana', 'orange', 'grape'];
+var position = fruits.indexOf('banana');
+console.log(position);
