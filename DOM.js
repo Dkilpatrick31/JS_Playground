55213cdea621ce943bc276e6d2c5dba74ffc2723
Dkilpatrick31 <dkilpatrick1990@gmail.com>
2019-09-24 19:20:51 -0500

    Created a for loop that iterates through the myList variable and makes every li element purple.

--- DOM.js
+++ DOM.js
@@ -1,10 +1,18 @@
 
 // Selecting the element
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+
+// const myHeading = document.getElementsByTagName('h1')[0];
+// const myButton = document.getElementById('myButton');
+// const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
 
 // Telling the element to listen for clicks and manipulate the element based off of the code inside the function.
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
-})
+
+// myButton.addEventListener('click', () => {
+//   myHeading.style.color = myTextInput.value;
+// })
+
+this for loop iterates through the myList variable and makes every li purple.
+for (let i=0; i < myList.length; i += 1) {
+  myList[i].style.color = 'purple';
+}
